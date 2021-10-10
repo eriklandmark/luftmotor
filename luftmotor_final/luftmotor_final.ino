82f06318680d94e6e50d13efcac935fe626a863f
Erik Landmark <erik@landmark.se>
2021-10-10 12:04:34 +0200

    worked on final version

--- luftmotor_final/luftmotor_final.ino
+++ luftmotor_final/luftmotor_final.ino
@@ -5,6 +5,9 @@
 
 #define PRINT_MICROS false
 
+#define INLET_VALVE_PIN 4
+#define OUTLET_VALVE_PIN 5
+
 MS5803 sensor(ADDRESS_HIGH);
 
 // Constants
@@ -13,22 +16,31 @@ const double stroke_volume = 3.110176727053895e-5;
 const double ambient_preassure = 1.0183748e5;
 const double pre_solenoid_starting_time = 7/960;
 const double solenoid_act_time = 21/960;
-
+unsigned long cykel_offset = 0;
 
 // Meassured value
 short rotation_velocity = 300;
+double pressure = 0;
 
+//Flags
+bool cycle_started = false;
+bool opened_outlet = false;
+bool closed_outlet = false;
+bool opened_inlet = false;
+bool closed_inlet = false;
+
+//Other
 int num_pressure_memory = 8;
 double pressure_memory[8] = {};
 byte pressure_memory_index = 0;
-double pressure = 0;
-
-unsigned long latest_micros = 0;
-unsigned long finish_cykel_micros = 0;
-bool cycle_started = false;
-
+unsigned long close_outlet_micros = 0;
+unsigned long open_inlet_micros = 0;
+unsigned long close_inlet_micros = 0;
 
 void setup() {
+    pinMode(INLET_VALVE_PIN, OUTPUT);
+    pinMode(OUTLET_VALVE_PIN, OUTPUT);
+    
     Timer1.initialize(6e7/rotation_velocity);
     Timer1.attachInterrupt(timeInterrupt);
     
@@ -42,15 +54,18 @@ void setup() {
 void timeInterrupt(void) {
     const unsigned long current_micros = micros();
     
-    if (latest_micros > 0) {
-      //Serial.println(current_micro - latest_micros);
-    }
-    double start_closing_inlet_percent = acos((2*tdc_volume + stroke_volume - (2*ambient_preassure*(tdc_volume + stroke_volume))/2e5)/stroke_volume)/(2*PI) - rotation_velocity*(pre_solenoid_starting_time - solenoid_act_time)/60;
     cycle_started = true;
-    latest_micros = current_micros;
-    finish_cykel_micros = current_micros + (start_closing_inlet_percent*6e7/rotation_velocity);
+    
+    double start_closing_inlet_percent = acos((2*tdc_volume + stroke_volume - (2*ambient_preassure*(tdc_volume + stroke_volume))/2e5)/stroke_volume)/(2*PI) - rotation_velocity*(pre_solenoid_starting_time - solenoid_act_time)/60;
+    close_outlet_micros = current_micros + cykel_offset;
+    open_inlet_micros = close_outlet_micros + (pre_solenoid_starting_time + solenoid_act_time)*1e6;
+    close_inlet_micros = open_inlet_micros + (start_closing_inlet_percent*6e7/rotation_velocity);
+    opened_outlet = false;
+    closed_outlet = false;
+    opened_inlet = false;
+    closed_inlet = false;
     Serial.print(current_micros);
-    Serial.print("\t");
+    Serial.print(" ");
     Serial.println(pressure);
 }
 
@@ -62,8 +77,18 @@ void loop() {
     interrupts();
     
     if (cycle_started_copy) {
-        if (current_micros >= finish_cykel_micros) {
+        if (current_micros >= close_outlet_micros && !closed_outlet) {
+            digitalWrite(OUTLET_VALVE_PIN, false);
+            closed_outlet = true;
+        } else if (current_micros >= open_inlet_micros && !opened_inlet) {
+            digitalWrite(INLET_VALVE_PIN, true);
+            opened_inlet = true;
+        } else if (current_micros >= close_inlet_micros && !closed_inlet) {
+            digitalWrite(INLET_VALVE_PIN, false);
+            closed_inlet = true;
             cycle_started = false;
+        } else {
+            
         }
     } else {
         pressure_memory[pressure_memory_index] = sensor.getPressure(ADC_2048)*100;
