6967ce191dc0c02eaaa1cbec77d21cf6d420489c
Erik Landmark <erik.landmark@itggot.se>
2021-11-02 21:46:23 +0100

    final version of code

--- luftmotor_final/luftmotor_final.ino
+++ luftmotor_final/luftmotor_final.ino
@@ -4,112 +4,212 @@
 #include <TimerOne.h>
 
 #define USE_PRESSURE_SENSOR false
+#define USE_TIMERS false
 
-#define PRINT_MICROS false
+#define PRINT_INFO false
 
 #define TDC_INTERRUPT_PIN 2
 #define BDC_INTERRUPT_PIN 3
+#define VEL_PIN 8
 
 #define INLET_VALVE_PIN 4
 #define OUTLET_VALVE_PIN 5
 
+#define START_MOTOR_BTN 6
+#define STOP_MOTOR_BTN 7
+
 MS5803 sensor(ADDRESS_HIGH);
 
 // Constants
-const double tdc_volume = 3.349518347057704e-7;
-const double stroke_volume = 3.110176727053895e-05;
-const double ambient_preassure = 1.0183748e5;
-const double pre_solenoid_starting_time = 7.0/960.0;
-const double solenoid_act_time = 21.0/960.0;
-const double cylinder_raduis = 15.0e-3;
-const double leverarm_length = 22.0e-3;
-const double connecting_rod_length = 90.0e-3;
-unsigned long cykel_offset = 0;
+const float tdc_volume = 3.349518347057704e-7;
+const float stroke_volume = 3.110176727053895e-05;
+const float ambient_pressure = 1e5; //1.0183748e5;
+const float pre_solenoid_starting_time = 7.0/960.0;
+const float solenoid_act_time = 23.0/960.0;
+const float cylinder_raduis = 15.0e-3;
+const float leverarm_length = 22.0e-3;
+const float connecting_rod_length = 90.0e-3;
+
+const float tdc_inlet_offset_angle = 70; 
+const float tdc_outlet_offset_angle =  0;
+const float bdc_cycle_offset_angle = 0;
 
 // Meassured value
-double rotation_velocity = 300.0;
-double pressure = 0;
+float rotation_velocity = 0.0;
+volatile unsigned long last_velocity_micros = 0;
+volatile short rotations = 0;
+unsigned short vel_changes = 0;
+double pressure = 2e5;
 
 //Flags
-bool cycle_started = false;
+bool motor_started = false;
+bool bdc_cycle_started = false;
+bool tdc_cycle_started = false;
 bool opened_outlet = false;
 bool closed_outlet = false;
 bool opened_inlet = false;
 bool closed_inlet = false;
+bool last_vel_state = false;
+bool tdc_happend = false;
+bool bdc_happend = false;
+
+const float min_inlet_percent = 0.2;
+const float max_inlet_percent = 0.3;
+const float min_rotation_velocity = 50.0;
+const float max_rotation_velocity = 1000.0;
 
 //Other
 int num_pressure_memory = 8;
 double pressure_memory[8] = {};
 byte pressure_memory_index = 0;
 
-unsigned long latest_micros = 0;
-unsigned long finish_cykel_micros = 0;
-unsigned long close_outlet_micros = 0;
-unsigned long open_inlet_micros = 0;
-unsigned long close_inlet_micros = 0;
+volatile unsigned long latest_micros = 0;
+volatile unsigned long finish_cykel_micros = 0;
+volatile unsigned long close_outlet_micros = 0;
+volatile unsigned long open_inlet_micros = 0;
+volatile unsigned long close_inlet_micros = 0;
+volatile unsigned long open_outlet_micros = 0;
+volatile unsigned long last_tdc_micros = 0;
+volatile unsigned long last_bdc_micros = 0;
 
 void setup() {
-    pinMode(INLET_VALVE_PIN, INPUT);
-    pinMode(OUTLET_VALVE_PIN, INPUT);
     pinMode(INLET_VALVE_PIN, OUTPUT);
     pinMode(OUTLET_VALVE_PIN, OUTPUT);
-
-    Timer1.initialize(6e7/rotation_velocity);
-    Timer1.attachInterrupt(timeInterrupt);
+    pinMode(TDC_INTERRUPT_PIN, INPUT);
+    pinMode(BDC_INTERRUPT_PIN, INPUT);
+    pinMode(START_MOTOR_BTN, INPUT);
+    pinMode(STOP_MOTOR_BTN, INPUT);
     
-    Wire.begin();
-    Serial.begin(9600);
 
+    if (USE_TIMERS) {
+        Timer1.initialize(6e7/rotation_velocity);
+        Timer1.attachInterrupt(tdcInterrupt);
+    } else {
+        attachInterrupt(digitalPinToInterrupt(TDC_INTERRUPT_PIN), tdcInterrupt, FALLING);
+        attachInterrupt(digitalPinToInterrupt(BDC_INTERRUPT_PIN), bdcInterrupt, FALLING);
+    }
+    
     if (USE_PRESSURE_SENSOR) {
+        Wire.begin();
         sensor.reset();
         sensor.begin();
-    } else {
-        pressure = 2e5;
+
+        delay(10);
+        pressure = sensor.getPressure(ADC_512)*100;
+    }
+    
+    Serial.begin(9600);
+}
+
+void tdcInterrupt(void) {
+    const unsigned long current_micros = micros();
+
+    if (current_micros - last_tdc_micros >= 1e5 && !digitalRead(TDC_INTERRUPT_PIN) && !tdc_happend) {
+        last_tdc_micros = current_micros;
+        tdc_cycle_started = true;
+        tdc_happend = true;
+        bdc_happend = false;
+        rotations++;
+        
+        float start_closing_inlet_percent = min(max_inlet_percent,max(min_inlet_percent,0.5 - 0.016666667*rotation_velocity * (pre_solenoid_starting_time + solenoid_act_time) - 0.15915494 * acos((0.00070685835*pressure * ((2001406.1 * pow(0.000031436719*ambient_pressure - 0.000079503087 * pressure, 2))/pow(pressure, 2) - 0.007616))/(0.0000013832156 * ambient_pressure - 0.0000034981358*pressure))));
+        
+        close_outlet_micros = current_micros + long (tdc_outlet_offset_angle/(6*rotation_velocity));
+        open_inlet_micros = current_micros + long (tdc_inlet_offset_angle/(6*rotation_velocity)); //- long ((pre_solenoid_starting_time + solenoid_act_time)*1.0e6);
+        close_inlet_micros = open_inlet_micros + long (start_closing_inlet_percent*6e7/rotation_velocity);
+        
+        opened_outlet = false;
+        closed_outlet = false;
+        opened_inlet = false;
+        closed_inlet = false;
+        
+        if (PRINT_INFO) {
+            Serial.print("TDC: ");
+            Serial.print(motor_started);
+            Serial.print("\t");
+            Serial.print(rotations);
+            Serial.print("\t");
+            Serial.print(rotation_velocity);
+            Serial.print("\t");
+            Serial.print(vel_changes);
+            Serial.print("\t");
+            Serial.print(pressure);
+            Serial.print("\t");
+            Serial.println(start_closing_inlet_percent,8);
+        }
+        vel_changes = 0;
     }
 }
 
-void timeInterrupt(void) {
+void bdcInterrupt(void) {
     const unsigned long current_micros = micros();
-    
-    cycle_started = true;
 
-    double sigma = tdc_volume*(pressure - ambient_preassure) + PI*pow(cylinder_raduis,2)*(leverarm_length*(pressure - 2*ambient_preassure) + connecting_rod_length*pressure);
-    double start_closing_inlet_percent = (acos(PI*pressure*pow(cylinder_raduis,2)*(pow(leverarm_length,2) - pow(connecting_rod_length,2) + pow(sigma,2)/(pow(pressure,2)*pow(cylinder_raduis,4)*pow(PI,2)))/(2*leverarm_length*sigma)) - PI*rotation_velocity*(solenoid_act_time + pre_solenoid_starting_time)/30.0)/(2.0*PI);
-    close_outlet_micros = current_micros + cykel_offset;
-    open_inlet_micros = close_outlet_micros + (pre_solenoid_starting_time + solenoid_act_time)*1e6;
-    close_inlet_micros = open_inlet_micros + (start_closing_inlet_percent*6e7/rotation_velocity);
-    opened_outlet = false;
-    closed_outlet = false;
-    opened_inlet = false;
-    closed_inlet = false;
-    Serial.print(micros() - current_micros);
-    Serial.print("\t");
-    Serial.println(start_closing_inlet_percent,8);
+    if (current_micros - last_bdc_micros >= 1e5 && !digitalRead(BDC_INTERRUPT_PIN) && !bdc_happend) {
+        last_bdc_micros = current_micros;
+        bdc_cycle_started = true;
+        opened_outlet = false;
+        tdc_happend = false;
+        bdc_happend = true;
+        
+        open_outlet_micros = current_micros + long (bdc_cycle_offset_angle/(6*rotation_velocity));
+        
+        if (PRINT_INFO) {
+            Serial.print("BDC: ");
+            Serial.print("\t");
+            Serial.print(motor_started);
+            Serial.print("\t");
+            Serial.println(rotation_velocity);
+        }
+    }
 }
 
 void loop() {
     const unsigned long current_micros = micros();
-    unsigned long cycle_started_copy;
-    noInterrupts();
-    cycle_started_copy = cycle_started;
-    interrupts();
-    
-    if (cycle_started_copy) {
-        if (current_micros >= close_outlet_micros && !closed_outlet) {
-            digitalWrite(OUTLET_VALVE_PIN, false);
-            closed_outlet = true;
-        } else if (current_micros >= open_inlet_micros && !opened_inlet) {
-            digitalWrite(INLET_VALVE_PIN, true);
-            opened_inlet = true;
-        } else if (current_micros >= close_inlet_micros && !closed_inlet) {
-            digitalWrite(INLET_VALVE_PIN, false);
-            closed_inlet = true;
-            cycle_started = false;
-        } else {
 
+    bool current_vel_state = digitalRead(VEL_PIN);
+    if (last_vel_state != current_vel_state) {
+      rotation_velocity = min(max_rotation_velocity, max(min_rotation_velocity, 2e7/(6*(current_micros - last_velocity_micros))));
+      
+      last_velocity_micros = current_micros;
+      last_vel_state = current_vel_state;
+      vel_changes++;
+    }
+    
+    unsigned long tdc_cycle_started_copy, bdc_cycle_started_copy;
+    noInterrupts();
+    tdc_cycle_started_copy = tdc_cycle_started;
+    bdc_cycle_started_copy = bdc_cycle_started;
+    interrupts();
+
+    if (motor_started) {
+        if (digitalRead(STOP_MOTOR_BTN)){
+            motor_started = false;
+            digitalWrite(INLET_VALVE_PIN, LOW);
+            digitalWrite(OUTLET_VALVE_PIN, LOW);
         }
-    } else {
-        if (USE_PRESSURE_SENSOR) {
+        if (tdc_cycle_started_copy) {
+            if (current_micros >= close_outlet_micros && !closed_outlet) {
+                digitalWrite(OUTLET_VALVE_PIN, LOW);
+                closed_outlet = true;
+            }
+            if (current_micros >= open_inlet_micros && !opened_inlet) {
+                digitalWrite(INLET_VALVE_PIN, HIGH);
+                opened_inlet = true;
+            }
+            if (current_micros >= close_inlet_micros && !closed_inlet) {
+                digitalWrite(INLET_VALVE_PIN, LOW);
+                closed_inlet = true;
+                tdc_cycle_started = false;
+            }
+        } else if (bdc_cycle_started_copy) {
+            if (current_micros >= open_outlet_micros && !opened_outlet) {
+                digitalWrite(OUTLET_VALVE_PIN, HIGH);
+                opened_outlet = true;
+                bdc_cycle_started = false;
+                if (USE_PRESSURE_SENSOR) {
+                    pressure = max(ambient_pressure + 100, sensor.getPressure(ADC_512)*100 - ambient_pressure);
+                }
+            }
+        } else if (USE_PRESSURE_SENSOR) {
             pressure_memory[pressure_memory_index] = sensor.getPressure(ADC_2048)*100;
             pressure_memory_index++;
             if (pressure_memory_index >= num_pressure_memory) {
@@ -119,7 +219,12 @@ void loop() {
             for (short i = 0; i < num_pressure_memory; i++) {
                 tot_pressure_memory += pressure_memory[i];
             }
-            pressure = tot_pressure_memory / (float) num_pressure_memory;
+            pressure = tot_pressure_memory / (float) num_pressure_memory - ambient_pressure;
+        }
+    } else {
+        if (digitalRead(START_MOTOR_BTN)) {
+            motor_started = true;
+            digitalWrite(OUTLET_VALVE_PIN, HIGH);
         }
     }
 }
