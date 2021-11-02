148dd7eab66812bf4cf3082263350e53a99b7a6c
Erik Landmark <erik.landmark@itggot.se>
2021-11-02 10:40:26 +0100

    final version

--- luftmotor_arduino/luftmotor_arduino.ino
+++ luftmotor_arduino/luftmotor_arduino.ino
@@ -2,21 +2,32 @@
 #include <SparkFun_MS5803_I2C.h>
 #include <math.h>
 
-#define TEST_SOLENOID false
+#define TEST_SOLENOID true
+#define TEST_BUTTON false
 #define TEST_MOTOR false
-#define TEST_LIGHT true
+#define TEST_LIGHT false
 #define TEST_SENSOR false
+#define TEST_VEL false
 
 MS5803 sensor(ADDRESS_HIGH);
 
-int led = LED_BUILTIN;
-int external_led = 8;
-int valve_pin = 7;
-int light_sensor_1 = A1;
-int light_sensor_2 = A2;
-int sensor_offset_pin = A0;
-int sensor_digital_pin_1 = 2;
-int sensor_digital_pin_2 = 3;
+#define TDC_INTERRUPT_PIN 2
+#define BDC_INTERRUPT_PIN 3
+#define VEL_PIN 8
+
+#define INLET_VALVE_PIN 4
+#define OUTLET_VALVE_PIN 5
+
+#define START_MOTOR_BTN 6
+#define STOP_MOTOR_BTN 7
+
+#define LIGHT_SENSOR_1 A0
+#define LIGHT_SENSOR_2 A1
+#define LIGHT_SENSOR_THRESHOLD A2
+
+#define VEL_SENSOR A3
+#define VEL_SENSOR_THESHOLD A4
+#define VEL_SENSOR_DIG 8
 
 int motorPin1 = 9; // Blue - 28BYJ48 pin 1
 int motorPin2 = 10; // Pink - 28BYJ48 pin 2
@@ -26,45 +37,53 @@ int motorSpeed = 800; //variable to set stepper speed
 int lookup[8] = {B01000, B01100, B00100, B00110, B00010, B00011, B00001, B01001};
 
 void setup() {
-    Wire.begin();
-    Serial.begin(9600);
-    pinMode(external_led, OUTPUT);
-    pinMode(valve_pin, OUTPUT);
-    pinMode(light_sensor_1, INPUT);
-    pinMode(light_sensor_2, INPUT);
-    pinMode(sensor_digital_pin_1, INPUT);
-    pinMode(sensor_digital_pin_2, INPUT);
-    pinMode(sensor_offset_pin, INPUT);
+    
+    pinMode(INLET_VALVE_PIN, OUTPUT);
+    pinMode(OUTLET_VALVE_PIN, OUTPUT);
+    pinMode(TDC_INTERRUPT_PIN, INPUT);
+    pinMode(BDC_INTERRUPT_PIN, INPUT);
+    pinMode(START_MOTOR_BTN, INPUT);
+    pinMode(STOP_MOTOR_BTN, INPUT);
+
+    
+    if (!TEST_SENSOR) {
+      pinMode(A4, INPUT);
+    }
     
     pinMode(motorPin1, OUTPUT);
     pinMode(motorPin2, OUTPUT);
     pinMode(motorPin3, OUTPUT);
     pinMode(motorPin4, OUTPUT);
 
-    sensor.reset();
-    sensor.begin();
+    if (TEST_SENSOR) {
+      Wire.begin();
+      sensor.reset();
+      sensor.begin();    
+    }
+    Serial.begin(9600);
+    
 }
 
 void loop() {
   if (TEST_SOLENOID) {
-    delay(4000);
-    digitalWrite(external_led, HIGH);
-    digitalWrite(valve_pin, HIGH);
-    delay(4000);
-    digitalWrite(external_led, LOW);
-    digitalWrite(valve_pin, LOW);
+    delay(1000);
+    digitalWrite(INLET_VALVE_PIN, HIGH);
+    digitalWrite(OUTLET_VALVE_PIN, HIGH);
+    delay(1000);
+    digitalWrite(INLET_VALVE_PIN, LOW);
+    digitalWrite(OUTLET_VALVE_PIN, LOW);
   }
   if (TEST_MOTOR){
     clockwise();
   }
   if (TEST_LIGHT) {
-    int light_sensor_val_1 = analogRead(light_sensor_1);
-    int light_sensor_val_2 = analogRead(light_sensor_2);
-    int voltage_val = analogRead(sensor_offset_pin);
-    int output_val_1 = digitalRead(sensor_digital_pin_1);
-    int output_val_2 = digitalRead(sensor_digital_pin_2);
-    Serial.print(micros());
-    Serial.print("\t");
+    int light_sensor_val_1 = analogRead(LIGHT_SENSOR_1);
+    int light_sensor_val_2 = analogRead(LIGHT_SENSOR_2);
+    int voltage_val = analogRead(LIGHT_SENSOR_THRESHOLD);
+    int output_val_1 = digitalRead(TDC_INTERRUPT_PIN);
+    int output_val_2 = digitalRead(BDC_INTERRUPT_PIN);
+    //Serial.print(micros());
+    //Serial.print("\t");
     Serial.print(voltage_val);
     Serial.print("\t");
     Serial.print(light_sensor_val_1);
@@ -75,7 +94,7 @@ void loop() {
     Serial.print("\t");
     Serial.println(output_val_2*30);
     if (!TEST_MOTOR) {
-      delay(1);
+      delayMicroseconds(1000);
     }
   }
   if (TEST_SENSOR) {
@@ -83,6 +102,28 @@ void loop() {
     //Serial.print(micros());
     //Serial.print("\t");
     Serial.println(pressure);
+  }
+  if (TEST_BUTTON) {
+    int output_val_1 = digitalRead(START_MOTOR_BTN);
+    int output_val_2 = digitalRead(STOP_MOTOR_BTN);
+    Serial.print(output_val_1);
+    Serial.print("\t");
+    Serial.println(output_val_2);
+    delay(500);
+  }
+  if (TEST_VEL) {
+    int analog_val = analogRead(VEL_SENSOR);
+    int digital_val = digitalRead(VEL_SENSOR_DIG);
+    if (!TEST_SENSOR) {
+      Serial.print(analogRead(VEL_SENSOR_THESHOLD));
+      Serial.print("\t");
+    }
+    Serial.print(analog_val);
+    Serial.print("\t");
+    Serial.println(digital_val*30);
+    if (!TEST_MOTOR) {
+      delay(1);
+    }
   }
 }
 
